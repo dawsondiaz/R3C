35cfeb547e045ea1ca1358deb7f6fe4a8121b715
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-14 19:52:27 -0700

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,16 @@
+var colors = {
+	BLACK:  0,
+	BROWN:  1,
+	RED:    2,
+	ORANGE: 3,
+	YELLOW: 4,
+	GREEN:  5,
+	BLUE:   6,
+	VIOLET: 7,
+	GRAY:   8,
+	WHITE:	9
+};
+
+function getResistance(bands, multiplier) {
+	
+}
