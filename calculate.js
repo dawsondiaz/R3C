84cf760055dabd6c43992a5664e89caee413c1f5
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-20 13:41:52 -0700

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,40 +1,39 @@
-var colors = {
-	"SILVER":	-2,
-	"GOLD":		-1,
-	//Silver and Gold (above) are only for multipliers
-	"BLACK":	0,
-	"BROWN":	1,
-	"RED":		2,
-	"ORANGE":	3,
-	"YELLOW":	4,
-	"GREEN":	5,
-	"BLUE":		6,
-	"VIOLET":	7,
-	"GRAY":		8,
-	"WHITE":	9
-};
+var colors = {};
+colors["SILVER"]	= -2;
+colors["GOLD"]		= -1;
+//Silver and Gold (above) are only for multipliers
+colors["BLACK"]		= 0;
+colors["BROWN"]		= 1;
+colors["RED"]		= 2;
+colors["ORANGE"]	= 3;
+colors["YELLOW"]	= 4;
+colors["GREEN"]		= 5;
+colors["BLUE"]		= 6;
+colors["VIOLET"]	= 7;
+colors["GRAY"]		= 8;
+colors["WHITE"]		= 9;
 
 function getResistanceStr(bands) {
 	var resistance = 
 		(
-			bands[0] * 10 +
-			bands[1]
+			colors[bands[0]] * 10 +
+			colors[bands[1]]
 		) *
-		Math.pow(10, bands[2])
+		Math.pow(10, colors[bands[2]])
 	;
 	
-	return resistance + "Ω " + tolerance(bands[3]) + "%";
+	return resistance + "Ω " + tolerance(colors[bands[3]]) + "%";
 }
 
 function tolerance(color) {
 	switch (color) {
-		case color."SILVER":	return 10;
-		case color."GOLD":		return 5;
-		case color."BROWN":		return 1;
-		case color."RED":		return 2;
-		case color."GREEN":		return 0.5;
-		case color."BLUE":		return 0.25;
-		case color."VIOLET":	return 0.1;
-		default: 				return 0;
+		case color.SILVER:	return 10;
+		case color.GOLD:	return 5;
+		case color.BROWN:	return 1;
+		case color.RED:		return 2;
+		case color.GREEN:	return 0.5;
+		case color.BLUE:	return 0.25;
+		case color.VIOLET:	return 0.1;
+		default: 			return 0;
 	}
 }
