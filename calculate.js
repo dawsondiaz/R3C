ce1495559004b70717c7ff268987efd75e23e010
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-20 13:26:05 -0700

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,20 +1,20 @@
 var colors = {
-	SILVER:	-2,
-	GOLD:	-1,
+	"SILVER":	-2,
+	"GOLD":		-1,
 	//Silver and Gold (above) are only for multipliers
-	BLACK:  0,
-	BROWN:  1,
-	RED:    2,
-	ORANGE: 3,
-	YELLOW: 4,
-	GREEN:  5,
-	BLUE:   6,
-	VIOLET: 7,
-	GRAY:   8,
-	WHITE:	9
+	"BLACK":	0,
+	"BROWN":	1,
+	"RED":		2,
+	"ORANGE":	3,
+	"YELLOW":	4,
+	"GREEN":	5,
+	"BLUE":		6,
+	"VIOLET":	7,
+	"GRAY":		8,
+	"WHITE":	9
 };
 
-function getResistanceStr(bands, multiplier) {
+function getResistanceStr(bands) {
 	var resistance = 
 		(
 			bands[0] * 10 +
@@ -28,13 +28,13 @@ function getResistanceStr(bands, multiplier) {
 
 function tolerance(color) {
 	switch (color) {
-		case color.SILVER:	return 10;
-		case color.GOLD:	return 5;
-		case color.BROWN:	return 1;
-		case color.RED:		return 2;
-		case color.GREEN:	return 0.5;
-		case color.BLUE:	return 0.25;
-		case color.VIOLET:	return 0.1;
-		default: 			return 0;
+		case color."SILVER":	return 10;
+		case color."GOLD":		return 5;
+		case color."BROWN":		return 1;
+		case color."RED":		return 2;
+		case color."GREEN":		return 0.5;
+		case color."BLUE":		return 0.25;
+		case color."VIOLET":	return 0.1;
+		default: 				return 0;
 	}
 }
