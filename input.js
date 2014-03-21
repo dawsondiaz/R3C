01222d64dc43c06274664bb99b1f2d627eaf2e83
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-21 13:03:19 -0700

    Update input.js

--- input.js
+++ input.js
@@ -4,19 +4,41 @@ $(document).ready(function() {
 
 $('select').change(function() {
 	switch ($(this).val()) {
-		case "SILVER":
+		case "SILVER":	$(this).css('background-color', 'gray');
+						$(this).css('color', 'white');
 			break;
-		case "GOLD":
+		case "GOLD":	$(this).css('background-color', 'yellow');
+						$(this).css('color', 'white');
 			break;
-		case "BLACK":
+		case "BLACK":	$(this).css('background-color', 'black');
+						$(this).css('color', 'white');
 			break;
-		case "BROWN":
+		case "BROWN":	$(this).css('background-color', 'brown');
+						$(this).css('color', 'white');
 			break;
-		case "RED":
+		case "RED":		$(this).css('background-color', 'red');
+						$(this).css('color', 'black');
 			break;
-			case "":
-			break;case "":
+		case "ORANGE":	$(this).css('background-color', 'orange');
+						$(this).css('color', 'black');
+			break;
+		case "YELLOW":	$(this).css('background-color', 'yellow');
+						$(this).css('color', 'black');
+			break;
+		case "GREEN":	$(this).css('background-color', 'green');
+						$(this).css('color', 'white');
+			break;
+		case "BLUE":	$(this).css('background-color', 'blue');
+						$(this).css('color', 'white');
+			break;
+		case "VIOLET":	$(this).css('background-color', 'purple');
+						$(this).css('color', 'white');
+			break;
+		case "GRAY":	$(this).css('background-color', 'gray');
+						$(this).css('color', 'white');
+			break;
+		case "WHITE":	$(this).css('background-color', 'white');
+						$(this).css('color', 'black');
 			break;
 	}
-	$(this).css();
 });
