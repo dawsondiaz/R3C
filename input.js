cf433c8567195337de35700c6cbc739e7e51100b
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-21 12:57:52 -0700

    Update input.js

--- input.js
+++ input.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-		
+	
+});
+
+$('select').change(function() {
+	switch ($(this).val()) {
+		case "SILVER":
+			break;
+		case "GOLD":
+			break;
+		case "BLACK":
+			break;
+		case "BROWN":
+			break;
+		case "RED":
+			break;
+			case "":
+			break;case "":
+			break;
+	}
+	$(this).css();
 });
