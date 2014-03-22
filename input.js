f0f60ccb1184a7763f01182d1bf2e99372d51329
FracturedRetina <admin@krakendev.net>
2014-03-22 10:40:05 -0700

    Color update

--- input.js
+++ input.js
@@ -1,15 +1,52 @@
 $(document).ready(function() {
-	// $('select').attr('onchange', 'selectChange()');
-});
+	$('select').change(function() {
+		switch ($(this).val()) {
+			case "SILVER":	$(this).css('background-color', 'gray');
+							$(this).css('color', 'white');
+				break;
+			case "GOLD":	$(this).css('background-color', 'yellow');
+							$(this).css('color', 'white');
+				break;
+			case "BLACK":	$(this).css('background-color', 'black');
+							$(this).css('color', 'white');
+				break;
+			case "BROWN":	$(this).css('background-color', 'brown');
+							$(this).css('color', 'white');
+				break;
+			case "RED":		$(this).css('background-color', 'red');
+							$(this).css('color', 'black');
+				break;
+			case "ORANGE":	$(this).css('background-color', 'orange');
+							$(this).css('color', 'black');
+				break;
+			case "YELLOW":	$(this).css('background-color', 'yellow');
+							$(this).css('color', 'black');
+				break;
+			case "GREEN":	$(this).css('background-color', 'green');
+							$(this).css('color', 'white');
+				break;
+			case "BLUE":	$(this).css('background-color', 'blue');
+							$(this).css('color', 'white');
+				break;
+			case "VIOLET":	$(this).css('background-color', 'purple');
+							$(this).css('color', 'white');
+				break;
+			case "GRAY":	$(this).css('background-color', 'gray');
+							$(this).css('color', 'white');
+				break;
+			case "WHITE":	$(this).css('background-color', 'white');
+							$(this).css('color', 'black');
+				break;
+		}
 
-function selectChange() {
-	alert("Resistance: " + getResistanceStr(
+
+		alert("Resistance: " + getResistanceStr(
 			new Array(
 				document.getElementById('band1').value,
 				document.getElementById('band2').value,
 				document.getElementById('band3').value,
 				document.getElementById('band4').value
 			)
-		)
-	);
-}
+		));
+	});
+});
