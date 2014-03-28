1c7336b15bd1240df94c8e78a175ef881712c305
Evan Shimoniak <eshimoniak@gmail.com>
2014-03-28 09:38:39 -0700

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -14,16 +14,40 @@ colors["GRAY"]		= 8;
 colors["WHITE"]		= 9;
 colors["NONE"]		= 0;
 
-function getResistanceStr(bands) {
-	var resistance = 
-		(
-			colors[bands[0]] * 10 +
-			colors[bands[1]]
-		) *
-		Math.pow(10, colors[bands[2]])
-	;
-	
-	return resistance + " ohms " + tolerance(colors[bands[3]]) + "%";
+function getResistanceStr(bands, numBands) {
+	switch (numBands) {
+		case 4:
+			var resistance = 
+				(
+					colors[bands[0]] * 10 +
+					colors[bands[1]]
+				) *
+				Math.pow(10, colors[bands[2]])
+			;
+			
+			return resistance + " ohms " + tolerance(colors[bands[3]]) + "%";
+		case 5:
+			var resistance = 
+				(
+					colors[bands[0]] * 100 +
+					colors[bands[1]] * 10 +
+					colors[bands[2]]
+				) *
+				Math.pow(10, colors[bands[3]])
+			;
+			
+			return resistance + " ohms " + tolerance(colors[bands[4]]) + "%";
+		case 6:
+			var resistance = 
+				(
+					colors[bands[0]] * 100 +
+					colors[bands[1]] * 10 +
+					colors[bands[2]]
+				) *
+				Math.pow(10, colors[bands[3]])
+			;
+			
+			return resistance + " ohms " + tolerance(colors[bands[4]]) + "% " + temp(colors[bands[5]] + "ppm");
 }
 
 function tolerance(color) {
@@ -31,10 +55,20 @@ function tolerance(color) {
 		case colors["SILVER"]:	return 10;
 		case colors["GOLD"]:	return 5;
 		case colors["BROWN"]:	return 1;
-		case colors["RED"]:		return 2;
+		case colors["RED"]:	return 2;
 		case colors["GREEN"]:	return 0.5;
 		case colors["BLUE"]:	return 0.25;
 		case colors["VIOLET"]:	return 0.1;
-		default: 			return 0;
+		default: 		return 0;
+	}
+}
+
+function temp(color) {
+	switch (color) {
+		case colors["BROWN"]:	return 100;
+		case colors["RED"]:	return 50;
+		case colors["ORANGE"]:	return 15;
+		case colors["YELLOW"]:	return 25;
+		default:		return 0;
 	}
 }
